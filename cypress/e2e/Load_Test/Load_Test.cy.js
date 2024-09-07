113050b306344bf770b88e5a5824e3ca0d92c10d
Ajay0930 <malik.ajay1992@yahoo.com>
2024-09-07 16:29:39 +0530

    Updated LoadTest File url

--- cypress/e2e/Load_Test/Load_Test.cy.js
+++ cypress/e2e/Load_Test/Load_Test.cy.js
@@ -4,7 +4,7 @@
 
 describe('Load Test Homepage', () => {
     it('should measure response time of the homepage', () => {
-      const url = 'http://www.automationpractice.pl/index.php'; 
+      const url = 'luxury.tatacliq.com'; 
   
       // Perform 1000 requests in a loop
       for (let i = 0; i < 1000; i++) {
@@ -12,10 +12,12 @@ describe('Load Test Homepage', () => {
           method: 'GET',
           url: url,
           timeout: 15000, // Set timeout to 15 seconds
+          failedonStatus: false 
         }).then((response) => {
           // Measure response time
           cy.log(`Response time for request ${i + 1}: ${response.duration} ms`);
           expect(response.status).to.eq(200); // Check for a 200 OK status
+
         });
       }
     });
